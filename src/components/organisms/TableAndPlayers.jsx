40eb391c5a7f38dfe8b6415afdd926e61513a4c6
Carlos Chamorro <carlosjairo38@gmail.com>
2024-07-30 15:06:34 -0500

    Commit de prueba

--- src/components/organisms/TableAndPlayers.jsx
+++ src/components/organisms/TableAndPlayers.jsx
@@ -1,19 +1,9 @@
 import Table from "../atoms/Table";
-import AdminTable from "../molecules/AdminTable";
 import UserItem from "../molecules/UserItem";
-import "../../styles/organisms/TableAndPlayers.css";
 import CurrentUserTable from "../atoms/CurrentUserTable";
 import Locker from "../molecules/Locker";
 import { useSelector } from "react-redux";
-
-const initialUsers = [
-  { id: "1", name: "name 1", rol: ["player"] },
-  { id: "2", name: "name 2", rol: ["player"] },
-  { id: "3", name: "name 3", rol: ["player"] },
-  { id: "4", name: "name 4", rol: ["player"] },
-  { id: "5", name: "name 5", rol: ["player"] },
-  { id: "6", name: "name 6", rol: ["player"] },
-];
+import "../../styles/organisms/TableAndPlayers.css";
 
 const TableAndPlayers = () => {
   const { players } = useSelector((state) => state.game);
@@ -27,7 +17,6 @@ const TableAndPlayers = () => {
 
   return (
     <section className="table-and-players">
-      {/* <AdminTable /> */}
       <CurrentUserTable />
 
       <Locker className="table-locker">
