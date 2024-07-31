0304e16790f92b35d0c59cc30386d43c4b97571a
Carlos Chamorro <carlosjairo38@gmail.com>
2024-07-31 15:28:09 -0500

    mostrar cargando y boton de reiniciar

--- src/components/molecules/Table.jsx
+++ src/components/molecules/Table.jsx
@@ -1,31 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../../styles/atoms/Table.css";
 import Button from "../atoms/Button";
 import { useDispatch, useSelector } from "react-redux";
 import { gameToFinish } from "../../reducers/game/gameSlice";
 
 const Table = ({ roles }) => {
+  const [loading, setLoading] = useState(false);
+  const [everyoneVoted, setEveryoneVoted] = useState(false);
   const isOwner = roles.includes("owner");
-  const { selectedCards, players } = useSelector((state) => state.game);
+  const { selectedCards, players, state } = useSelector((state) => state.game);
 
   const dispatch = useDispatch();
-  let everyoneVoted;
 
-  if (isOwner) {
-    // everyoneVoted = players.length === selectedCards.length;
-    everyoneVoted = true;
-  }
+  useEffect(() => {
+    if (isOwner) {
+      setEveryoneVoted(true);
+    }
+  }, [isOwner, players, selectedCards]);
 
   const showCards = () => {
-    dispatch(gameToFinish());
+    setLoading(true);
+    setEveryoneVoted(false);
+
+    setTimeout(() => {
+      setLoading(false);
+      dispatch(gameToFinish());
+    }, 300);
   };
 
   return (
     <div className="table">
-      {isOwner && everyoneVoted && (
+      {loading && <p>Cargando...</p>}
+      {isOwner && everyoneVoted && !loading && (
         <Button onClick={showCards} className={"show-cards-btn"}>
           Revelar cartas
         </Button>
+      )}
+      {state === "finished" && !loading && (
+        <button className={"show-results-btn"}>Mostrar resultados</button>
       )}
     </div>
   );
