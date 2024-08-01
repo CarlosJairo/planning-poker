b22e075d43810c437a4db1bddc4126e3638038c0
Carlos Chamorro <carlosjairo38@gmail.com>
2024-08-01 17:29:42 -0500

    desarrollando funcionalidades HU7

--- src/components/pages/GameTableScreen.jsx
+++ src/components/pages/GameTableScreen.jsx
@@ -1,25 +1,40 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import Modal from "../organisms/Modal";
+import Modal from "./Modal";
 import HeaderTableScreen from "../organisms/HeaderTableScreen";
 import TableAndPlayers from "../organisms/TableAndPlayers";
 import SelectableCardContainer from "../organisms/SelectableCardContainer";
 import CardResultsCtn from "../organisms/CardResultsCtn";
 import "../../styles/pages/GameTableScreen.css";
+import useModal from "../../hooks/useModal";
+import UserForm from "../organisms/FormUser";
+import ModalCopyLinkContent from "../molecules/ModalCopyLinkContent";
 
 const GameTableScreen = () => {
-  const [showModal, setShowModal] = useState(true);
+  const [modalForm, setModalForm] = useModal(true);
+  const [modalLink, setModalLink, toggleModalLink] = useModal(false);
+
   const { poolCards, state } = useSelector((state) => state.game);
 
   return (
     <section className="game-table-screen">
-      {showModal && <Modal setShowModal={setShowModal} />}
-      <HeaderTableScreen />
+      <HeaderTableScreen toggleModalLink={toggleModalLink} />
       <TableAndPlayers />
       {state === "revealed_cards" ? (
         <CardResultsCtn />
       ) : (
         <SelectableCardContainer poolCards={poolCards} />
+      )}
+      {modalForm && (
+        <Modal isOpen={modalForm}>
+          <UserForm setModalForm={setModalForm} />
+        </Modal>
+      )}
+
+      {modalLink && (
+        <Modal isOpen={modalForm}>
+          <ModalCopyLinkContent toggleModalLink={toggleModalLink} />
+        </Modal>
       )}
     </section>
   );
